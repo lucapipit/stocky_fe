cd6c2815d2298a6535f7c7b8fede28f0fe14394d
oussein <ouzinnbis@hotmail.it>
2023-12-27 11:02:24 +0100

    aggunti codice footer and account

--- src/components/_Footer.jsx
+++ src/components/_Footer.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import '../styles/footer.css'
 import Logo from '../assets/zeus2.jpg'
+import { FaFacebook, FaTwitter, FaPinterest, FaInstagram } from 'react-icons/fa'
 
 const _Footer = () => {
   return (
     <div className='footer'>
          <img src={Logo} alt="logo" />
-         <span>Made with our Stocky</span>
+         {/* <span>Made with our Stocky</span> */}
+         <small className='website-rights'>infodent © 2023</small>
+
+         <div className="footerItem">
+          <span className="footerTitle">FOLLOW US</span>
+          <div className="footerSocial">
+            <i className="footerIcon"><FaFacebook/></i>
+            <i className="footerIcon"><FaTwitter /></i>
+            <i className="footerIcon">< FaPinterest/></i>
+            <i className="footerIcon"><FaInstagram /></i>
+          </div>
+          </div>
     </div>
+
+    
   )
 }
 
