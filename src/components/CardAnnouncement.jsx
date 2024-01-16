7337d0d1e544ceeb29f1efdd1dd171ae1563dabd
lucapipit <pipitonelucamaria@gmail.com>
2024-01-16 17:54:35 +0100

    aggiunto un commento

--- src/components/CardAnnouncement.jsx
+++ src/components/CardAnnouncement.jsx
@@ -58,10 +58,10 @@ const CardAnnouncement = ({ singleData, isLoading }) => {
                         <div className='d-flex align-items-center justify-content-between'>
                             <div className='d-flex align-items-top my-3'>
                                 <h2 className='fw-normal'>{singleData[0].price}</h2>
-                                <p className='mt-1'>00$</p>
+                                <p className='fw-light'>00$</p>
                             </div>
                             <div className='d-flex align-items-top my-3 px-3 py-1 rounded-5 myBgWhite'>
-                                <h2 className='fw-light'>{(Math.round((singleData[0].price) / (singleData[0].quantity) * 100) / 100).toString().split(".")[0]}</h2>
+                                <h2 className='fw-normal'>{(Math.round((singleData[0].price) / (singleData[0].quantity) * 100) / 100).toString().split(".")[0]}</h2>
                                 <p className='fw-light'>{(Math.round((singleData[0].price) / (singleData[0].quantity) * 100) / 100).toString().split(".")[1]}$</p>
                                 <h4 className='fw-light'>/item</h4>
                             </div>
