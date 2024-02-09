a207b33dd642d0b26297d971a8e570e5de6a8b95
oussein <ouzinnbis@hotmail.it>
2024-02-09 18:49:06 +0100

    lavorazione stripe payment

--- root/src/components/PaymentAnnouncement.jsx
+++ root/src/components/PaymentAnnouncement.jsx
@@ -3,7 +3,7 @@ import StripeCkechout from "react-stripe-checkout";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { PaymentFunc } from "../states/paymentState";
 
 
@@ -27,7 +27,7 @@ const PaymentAnnouncement = () => {
 
     const handleToken = (token) => {
         setToken(token);
-        dispatch(PaymentFunc({tkn: token, apiKey: KEY}));
+        dispatch(PaymentFunc({ tkn: token, apiKey: KEY }));
         setPayment(true);
     }
 
@@ -36,39 +36,41 @@ const PaymentAnnouncement = () => {
     }
 
     return (
-
         <div className="container">
             <div className="row">
                 <div className="col-12 col-md-6">
                     <CardAnnouncement singleData={singleData} isLoading={isLoading} />
                 </div>
                 <div className="col-12 col-md-6">
-                    <div className="d-flex justify-content-center align-items-center flex-column">
-                        <h3 className="text-center">Payment</h3>
-                        <div className="d-flex justify-content-center align-items-center flex-column">
-                            <h5 className="text-center">
-                                Total:{" "}
-                                {singleData[0] && !isLoading
-                                    ? singleData[0].price + "$"
-                                    : "Loading..."}
-                            </h5>
-                            <StripeCkechout
-                                token={handleToken}
-                                stripeKey={KEY}
-                                amount={singleData[0] && !isLoading ? singleData[0].price * 100 : 0}
-                                name="infodent"
-                                image="https://avatars.githubusercontent.com/u/25126281?v=4"
-                                billingAddress
-                                shippingAddress
-                            />
-                            <Button variant="primary" onClick={() => navigate("/store")}>
-                                Back to store
-                            </Button>
-                        </div>
-                    </div>
+                    <Card className="p-3">
+                        <Card.Body className="d-flex justify-content-center align-items-center flex-column">
+                            <Card.Title className="text-center">Payment</Card.Title>
+                            <div className="d-flex justify-content-center align-items-center flex-column">
+                                <Card.Subtitle className="mb-2 text-muted text-center">
+                                    Total:{" "}
+                                    {singleData[0] && !isLoading
+                                        ? singleData[0].price + "$"
+                                        : "Loading..."}
+                                </Card.Subtitle>
+                                <StripeCkechout
+                                    token={handleToken}
+                                    stripeKey={KEY}
+                                    amount={singleData[0] && !isLoading ? singleData[0].price * 100 : 0}
+                                    name="infodent"
+                                    image="https://avatars.githubusercontent.com/u/25126281?v=4"
+                                    billingAddress
+                                    shippingAddress
+                                />
+                                <Button variant="primary mt-3" onClick={() => navigate("/store")}>
+                                    Back to store
+                                </Button>
+                            </div>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </div>
+
 
     );
 }
